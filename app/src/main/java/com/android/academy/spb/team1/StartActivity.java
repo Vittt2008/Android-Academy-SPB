6f7e1d8e6005524fce5c5dec2b7dc880df699655
Anastasia Kavunskaya <kavunskanas@gmail.com>
2018-06-10 20:55:16 +0300

    activity start added

---
+++ app/src/main/java/com/android/academy/spb/team1/StartActivity.java
@@ -0,0 +1,12 @@
+package com.android.academy.spb.team1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
