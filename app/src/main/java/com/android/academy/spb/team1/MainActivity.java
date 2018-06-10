8f0de0d25c5466ec310ca92afd95ee212328c21c
valentinet <valentine.timinsky@gmail.com>
2018-06-10 20:58:15 +0300

    Extra fix

--- app/src/main/java/com/android/academy/spb/team1/MainActivity.java
+++ app/src/main/java/com/android/academy/spb/team1/MainActivity.java
@@ -98,8 +98,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (locationManager.getBestProvider(new Criteria(), true) != null) {
-            lastKnownLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), true)
-            );
+            lastKnownLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), true));
         }
 
         String forecastURL = String.format(
